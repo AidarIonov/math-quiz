0c5d3b140d146606dd5d59d79e954ebc0dd3001e
Aidar <aidarionov@gmail.com>
2022-08-09 11:20:02 +0600

    feat: footer and image minify plugin

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,11 +1,54 @@
-const { merge } = require('webpack-merge')
-const baseWebpackConfig = require('./webpack.base.conf')
-
+const { merge } = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.conf');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const { extendDefaultPlugins } = require('svgo');
 const buildWebpackConfig = merge(baseWebpackConfig, {
   mode: 'production',
-  plugins: []
-})
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      '...',
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+              [
+                'svgo',
+                {
+                  plugins: extendDefaultPlugins([
+                    {
+                      name: 'removeViewBox',
+                      active: false,
+                    },
+                    {
+                      name: 'addAttributesToSVGElement',
+                      params: {
+                        attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+                      },
+                    },
+                  ]),
+                },
+              ],
+            ],
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [],
+});
 
 module.exports = new Promise((resolve, reject) => {
-  resolve(buildWebpackConfig)
-})
+  resolve(buildWebpackConfig);
+});
